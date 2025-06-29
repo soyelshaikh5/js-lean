a5dcf8dc1623133141c731179b395215d78dea35
soyelshaikh5 <soyel.shaikh126@gmail.com>
2025-06-29 08:27:21 +0000

    updated the stack and heap example

---
+++ 01_basics/comparison.js
@@ -0,0 +1,32 @@
+// let score = null
+
+// console.log(score)
+
+// console.log(score >= 0)
+
+// console.log(null == 0)
+
+// let numberScore = Number(score)
+
+// console.log(numberScore)
+
+// console.log(2 > 1)
+// console.log(1 != 5)
+
+
+// console.log('a' == 'a')
+
+// ===
+
+// console.log("2" === 2);
+
+let bigNumber = 12344567897654322n
+
+// console.log(typeof bigNumber);
+
+
+const myFunction = function(){
+    console.log("Hello World");
+}
+
+console.log(typeof myFunction)
---
+++ 01_basics/stack-heap_memory.js
@@ -0,0 +1,23 @@
+let city = "Mumbai"
+
+console.log(city);
+
+
+newCity = city
+newCity = "Banglore"
+
+console.log(newCity);
+
+
+let userOne = {
+    email : "Steven@gmail.com",
+    upi : "Steven@ybl",
+}
+
+console.log(userOne.email)
+
+let userTwo = userOne
+userTwo.email = "Mark@gmail.com"
+
+console.log(userTwo.email)
+console.log(userOne.email)
